a15a82e9c4313ee458f28060d2305eaa04ef4852
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-12 16:32:33 +0200

    TOURLY-2 - Added globe asset and implemented AppLogoWithText global reusable component

---
+++ app/src/main/java/com/tourly/app/core/ui/components/foundation/AppLogoWithText.kt
@@ -0,0 +1,37 @@
+package com.tourly.app.core.ui.components.foundation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.tourly.app.R
+import com.tourly.app.core.ui.theme.OutfitFamily
+
+@Composable
+fun AppLogoWithText(
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = 32.sp
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AppLogo()
+
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontFamily = OutfitFamily,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = fontSize,
+            color = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
