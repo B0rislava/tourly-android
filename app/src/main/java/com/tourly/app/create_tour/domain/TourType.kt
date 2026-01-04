70119f381dbaf04aa6eb05415d480a574c9ff2e6
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2026-01-04 17:41:26 +0200

    TOURLY-10 - Added TourType enum & DayProgramme data class

---
+++ app/src/main/java/com/tourly/app/create_tour/domain/TourType.kt
@@ -0,0 +1,6 @@
+package com.tourly.app.create_tour.domain
+
+enum class TourType {
+    SINGLE_DAY,
+    MULTI_DAY
+}
---
+++ app/src/main/java/com/tourly/app/create_tour/presentation/state/DayProgramme.kt
@@ -0,0 +1,6 @@
+package com.tourly.app.create_tour.presentation.state
+
+data class DayProgramme(
+    val dayNumber: Int,
+    val description: String
+)
