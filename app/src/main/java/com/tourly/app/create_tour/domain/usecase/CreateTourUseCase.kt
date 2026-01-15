bba889e5237a22fb1658c9e2fae02542154b3d67
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2026-01-15 12:14:58 +0200

    TOURLY-11 - Migrated existing guide features to domain model
    - Updated `TourRepository` interface to return `Tour` domain model instead of DTO
    - Refactored `TourRepositoryImpl` to use `TourMapper`
    - Updated `GuideHomeUiState` to hold `Tour` objects
    - Ensured consistent use of domain models across guide and traveler features

--- app/src/main/java/com/tourly/app/create_tour/domain/usecase/CreateTourUseCase.kt
+++ app/src/main/java/com/tourly/app/create_tour/domain/usecase/CreateTourUseCase.kt
@@ -1,11 +1,11 @@
 package com.tourly.app.create_tour.domain.usecase
 
-import com.tourly.app.core.network.model.CreateTourResponseDto
 import com.tourly.app.core.util.flatMap
 import com.tourly.app.create_tour.domain.mapper.CreateTourMapper
 import com.tourly.app.create_tour.domain.model.CreateTourParams
 import com.tourly.app.create_tour.domain.repository.TourRepository
 import com.tourly.app.create_tour.domain.validator.CreateTourValidator
+import com.tourly.app.home.domain.model.Tour
 import javax.inject.Inject
 
 class CreateTourUseCase @Inject constructor(
@@ -13,7 +13,7 @@ class CreateTourUseCase @Inject constructor(
     private val validator: CreateTourValidator,
     private val mapper: CreateTourMapper
 ) {
-    suspend operator fun invoke(params: CreateTourParams): Result<CreateTourResponseDto> {
+    suspend operator fun invoke(params: CreateTourParams): Result<Tour> {
         return validator.validate(params)
             .map { mapper.toDto(params) }
             .flatMap { repository.createTour(it) }
